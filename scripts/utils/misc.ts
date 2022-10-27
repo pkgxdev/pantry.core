5c7b1efd7a317599399f9513bdd7119c32e9959b
Max Howell <mxcl@me.com>
2022-10-27 16:07:37 -0400

    wip

--- scripts/utils/misc.ts
+++ scripts/utils/misc.ts
@@ -3,6 +3,9 @@ import { usePrefix } from "hooks"
 export async function overlay_this_pantry() {
   const pantry_prefix = usePrefix().join("tea.xyz/var/pantry")
   const self = new URL(import.meta.url).path().parent().parent().parent().join("projects")
+
+  if (self.parent().eq(pantry_prefix)) return
+
   const to = pantry_prefix.join("projects")
   for await (const [path, {isFile}] of self.walk()) {
     if (isFile) {
