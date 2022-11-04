0ff6218df01365649792851ca5ba42464c29bbbc
Max Howell <mxcl@me.com>
2022-11-04 10:38:40 +0000

    Relax requirement to build to /opt
    Improve README somewhat. Don’t overlay files into pantry, use `TEA_PANTRY_PATH` instead. Requires tea/cli 0.11.6 for `env` key in YAML-FM

--- scripts/utils/misc.ts
+++ scripts/utils/misc.ts
@@ -1,17 +1,5 @@
-import { usePrefix } from "hooks"
-
-export async function overlay_this_pantry() {
-  const pantry_prefix = usePrefix().join("tea.xyz/var/pantry")
+export function overlay_this_pantry() {
   const self = new URL(import.meta.url).path().parent().parent().parent().join("projects")
 
-  // noop, but also THIS FUCKS SHIT UP
-  if (self.parent().eq(pantry_prefix)) return
-
-  const to = pantry_prefix.join("projects")
-  for await (const [path, {isFile}] of self.walk()) {
-    if (isFile) {
-      const dst = to.join(path.relative({ to: self }))
-      path.cp({ into: dst.parent().mkpath() })
-    }
-  }
+  Deno.env.set("TEA_PANTRY_PATH", self.string)
 }
