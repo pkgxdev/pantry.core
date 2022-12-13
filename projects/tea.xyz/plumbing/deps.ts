7d19d12aa5cc5bf593e7bf484979fb9e13ff0314
Max Howell <mxcl@me.com>
2022-12-13 16:41:33 -0500

    starting tea/cmd

---
+++ projects/tea.xyz/plumbing/deps.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env deno
+
+/*---
+args:
+  - deno
+  - run
+  - --allow-read={{tea.prefix}}/
+  - --allow-env
+  - --import-map=https://raw.githubusercontent.com/teaxyz/cli/main/import-map.json
+---*/
+
+import { Package, PackageRequirement } from "types"
+import { usePantry, useFlags } from "hooks"
+import { hydrate } from "prefab"
+import * as ARGV from "./utils/args.ts"
+import { set_output } from "./utils/gha.ts"
+import { pkg } from "utils"
+
+const pantry = usePantry()
+
+useFlags()
+
+const mode: 'build' | 'install' = 'build' //Deno.args.includes("-b") ? 'build' : 'install'
+
+const get_deps = async (pkg: Package | PackageRequirement) => {
+  const deps = await pantry.getDeps(pkg)
+  switch (mode) {
+  case 'build':
+    return [...deps.build, ...deps.runtime]
+  // case 'install':
+  //   return deps.runtime
+  }
+}
+
+const rv: PackageRequirement[] = []
+for await (const pkg of ARGV.pkgs()) {
+  const deps = await get_deps(pkg)
+  const wet = await hydrate(deps)
+  rv.push(...wet.pkgs)
+}
+
+const gas = rv.map(pkg.str)
+
+if (Deno.env.get("GITHUB_ACTIONS")) {
+  set_output("pkgs", gas)
+} else {
+  console.log(gas.join("\n"))
+}
