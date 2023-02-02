ad27386caaf9bfddca51b3f952d85896a513ee64
Jacob Heider <jacob@tea.xyz>
2023-02-01 22:48:25 -0500

    implement testMatrix

--- scripts/get-platform.ts
+++ scripts/get-platform.ts
@@ -13,35 +13,43 @@ import { panic } from "utils";
 
 const platform = Deno.env.get("PLATFORM") ?? panic("$PLATFORM not set")
 
-let container = "~"
-let os = ""
-let buildOs = ""
+let os: string | string[]
+let buildOs: string | string[]
+let testMatrix: { os: string | string[], container: string | undefined }[]
 
 switch(platform) {
   case "darwin+x86-64":
     os = "macos-11"
-    buildOs = "[self-hosted, macOS, X64]"
+    buildOs = ["self-hosted", "macOS", "X64"]
+    testMatrix = [{ os, container: undefined }]
     break
   case "darwin+aarch64":
-    os = "[self-hosted, macOS, ARM64]"
+    os = ["self-hosted", "macOS", "ARM64"]
     buildOs = os
+    testMatrix = [{ os, container: undefined }]
     break
   case "linux+aarch64":
-    os = "[self-hosted, linux, ARM64]"
+    os = ["self-hosted", "linux", "ARM64"]
     buildOs = os
+    testMatrix = [{ os, container: undefined }]
     break
   case "linux+x86-64":
-    // FIXME: we'd like to build on a number of different outputs
-    // [ubuntu, debian, ubuntu+infuser]
     os = "ubuntu-latest"
-    buildOs = "[self-hosted, linux, X64]"
-    container = "debian:buster-slim"
+    buildOs = ["self-hosted", "linux", "X64"]
+    testMatrix = [
+      { os, container: undefined },
+      { os: buildOs, container: undefined },
+      { os, container: "ghcr.io/teaxyz/infuser:latest" },
+      { os, container: "debian:buster-slim" },
+    ]
     break
   default:
     panic(`Invalid platform description: ${platform}`)
 }
 
-const output = `os=${os}\nbuild-os=${buildOs}\ncontainer=${container}\n`
+const output = `os=${JSON.stringify(os)}\n` +
+  `build-os=${JSON.stringify(buildOs)}\n` +
+  `testMatrix=${JSON.stringify(testMatrix)}\n`
 
 Deno.stdout.write(new TextEncoder().encode(output))
 
